f38fff1c5ddcc4870edf36b3c5c792d255ccad4c
031302625 <441703074@qq.com>
2015-12-12 21:54:19 +0800

    客户端最新源码，post  bug修复

--- JourneyHepler-master/SwiftSideslipLikeQQ/RightViewController.swift
+++ JourneyHepler-master/SwiftSideslipLikeQQ/RightViewController.swift
@@ -40,50 +40,44 @@ class RightViewController: UIViewController {
         
         
         
-        let parameters = [
-            "userId": "1",
-            "title":"\(titleText)",
-            "beginTime":"2015-12-27 12:00:00",
-            "endTime":"2015-12-27 12:00:00",
-            "summary":"\(planText)",
-            "article":"行程描述",
-            "routeImageUrl":"http://img.517huwai.com/Public/Uploads/2015/10/27/562f2af0d6762.jpg@560w_1000h",
-            "type":"201",
-            "secnics":"途径景点用分号隔开",
-            "assemblingPlace":"\(placeText)",
-            "strengthGrade":"活动强度",
-        ]
-        
-
-        
-        Alamofire.request(.POST, "http://120.27.34.200/JourneyHelper-Web/createRoute", parameters:parameters,encoding: .URL)
-            .responseString { response in
-                print(response.request)
-                print(response.response)
-                print(response.result)
-                print(response.debugDescription)
-                print(Alamofire.URLStringConvertible)
-        }
-
-//        let URL = NSURL(string: "http://120.27.34.200/JourneyHelper-Web/createRoute")!
-//        var request = NSMutableURLRequest(URL: URL)
+//        let parameters = [
+//            "userId": "1",
+//            "title":"\(titleText)",
+//            "beginTime":"2015-12-27 12:00:00",
+//            "endTime":"2015-12-27 12:00:00",
+//            "summary":"\(planText)",
+//            "article":"行程描述",
+//            "routeImageUrl":"http://img.517huwai.com/Public/Uploads/2015/10/27/562f2af0d6762.jpg@560w_1000h",
+//            "type":"201",
+//            "secnics":"途径景点用分号隔开",
+//            "assemblingPlace":"\(placeText)",
+//            "strengthGrade":"活动强度",
+//        ]
 //        
-//        let encoding = Alamofire.ParameterEncoding.URL
-//        (request, _) = encoding.encode(request, parameters: parameters)
-//        print(encoding)
-      
-        
-//        Alamofire.request(.POST, "http://120.27.34.200/JourneyHelper-Web/createRoute?userId=1&title=行程标题&beginTime=2015-12-24 08:00:00&endTime=2015-12-27 12:00:00&summary=行程摘要&article=行程详情描述&routeImageUrl=http://img.517huwai.com/Public/Uploads/2015/10/27/562f2af0d6762.jpg@560w_1000h&routeImageUrl=100&secnics=途径景点用分号隔开&assemblingPlace=集合地点&strengthGrade=活动强度")
-
-//        let URL = NSURL(string: "http://172.50.180.239//createRoute?userId=\(userId)&title=\(titleText)&beginTime=\(timeText)&endTime=nil&summary=nil&article=\(planText)&routeImageUrl=nil&type=nil&secnics=nil&assemblingPlace=\(placeText)&strengthGrade=nil")!
-//        let request = NSMutableURLRequest(URL: URL)
-//        print("数据:\(URL)")
-//        Alamofire.request(.GET, request)
-//            .responseJSON
-//        {
-//            response in
-//                debugPrint(response)
+//
+//        
+//        Alamofire.request(.POST, "http://120.27.34.200/JourneyHelper-Web/createRoute", parameters:parameters,encoding: .URL)
+//            .responseString { response in
+//                print(response.request)
+//                print(response.response)
+//                print(response.result)
+//                print(response.debugDescription)
+//                print(Alamofire.URLStringConvertible)
 //        }
+
+        
+       let hostString = host!
+       var str = "\(hostString)/createRoute?userId=\(userId)&title=\(titleText)&beginTime=&endTime=&summary=&article=\(planText)&routeImageUrl=&type=&secnics=&assemblingPlace=\(placeText)&strengthGrade="
+        str =  str.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        let URL =  NSURL(string:str)!
+        let request = NSMutableURLRequest(URL: URL)
+        print("数据:\(URL)")
+        Alamofire.request(.GET, request)
+            .responseJSON
+        {
+            response in
+                debugPrint(response)
+        }
         
         
         
